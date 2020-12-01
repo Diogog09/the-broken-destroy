c9b33c6f91233a620f33a4927da9e07c46838678
Diogog09 <55158232+Diogog09@users.noreply.github.com>
2020-11-30 22:12:28 -0300

    Add files via upload

---
+++ FirebaseConfig.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAStCnumhjmAxnn9Z7ZJNeuv0ZFt_yAi7M",
+    authDomain: "thebrokendestoy.firebaseapp.com",
+    databaseURL: "https://thebrokendestoy.firebaseio.com",
+    projectId: "thebrokendestoy",
+    storageBucket: "thebrokendestoy.appspot.com",
+    messagingSenderId: "476959636291",
+    appId: "1:476959636291:web:b046b96c3c0b6a1f630df6"
+  };
+
+  export default config;
